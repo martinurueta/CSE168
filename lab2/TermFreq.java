2519fe6b1f17dcba9ec0e5a380db3ae447fbbfad
martinurueta <71405406+martinurueta@users.noreply.github.com>
2023-01-30 15:51:34 -0800

    forgot to add

--- lab2/TermFreq.java
+++ lab2/TermFreq.java
@@ -11,7 +11,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import java.util.Arrays; // need to be able to resize our array that was used on line 53
 
-public class TermFrequency {
+public class TermFreq {
     public static class TokenizerMapper
        extends Mapper<Object, Text, Text, Text>{ // input text , output text , text
 
@@ -29,10 +29,10 @@ public class TermFrequency {
                 String[] array = temp_word.split(","); // to spilt up to find the index example "1,(word)" into a string array ["1","(word)"]
                 word = new Text(array[1]); // turn string of word into Text variable
                 id = new Text(array[0]); // turn string index into Text variable
-                context.write(id, word); // emit(word, index)
+                context.write(word, id); // emit(word, index)
         }
         else{
-                context.write(id, word); // emit(word, index)
+                context.write(word, id); // emit(word, index)
         }
         i++;
 
@@ -45,18 +45,33 @@ public class TermFrequency {
     public void reduce(Text key, Iterable<Text> values,
                        Context context
                        ) throws IOException, InterruptedException {
-                        for (Text val : values){
-                            String input = val.toString(); // turn Text id to string input
-                            
-                         }
+        int[] id_total = new int[1]; // make an array to hold the total # that the word appear in each line
+        for (Text val : values){
+                String input = val.toString(); // turn Text id to string input
+                int id = Integer.parseInt(input); // turn string input to int id
+                if (id_total.length < id){ // if the index is greater than the length of the array that is defaulted to size of 1
+                    id_total = Arrays.copyOf(id_total, id); // we change the size of the array to the size of that id
+                }
+                id_total[id-1] = id_total[id-1] + 1; // we add +1 to the total number # that word appear in each line (array [0] = line 1 on the text, array [1]= line 2 on the text, ...)
+        }
+        for (int i = 0; i < id_total.length; i++){
+                Integer g = new Integer(i+1); // turn int i+1 to a integer g
+                String output_id = g.toString(); // turn integer g to string output_id to get the index of that line
+                Integer j = new Integer(id_total[i]); // turn int id_total[i] to an integer j
+                String output_sum = j.toString(); // turn integer j to output_sum to get the # of word that appear on that line
+                result = new Text(output_id+", "+output_sum); // combine output_id and , and ouput_sum == "index, #ofWordAppear" and turn it into Text result
+                if (id_total[i] > 0){// if the number of times that word appears is 0 or less then we dont emit
+                        context.write(key, result); // emit(word, (index, #ofWordAppear))
+                }
+        }
 
     }
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "Term Frequency");
-    job.setJarByClass(TermFrequency.class);
+    Job job = Job.getInstance(conf, "TermFreq");
+    job.setJarByClass(TermFreq.class);
     job.setMapperClass(TokenizerMapper.class);
 //    job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
